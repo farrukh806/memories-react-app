9eb3b877d122f85d21debdd3cba1111bf0f7762f
Farrukh Jamal <52830299+farrukh806@users.noreply.github.com>
2021-05-24 21:08:54 +0530

    Added page details route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,9 +2,49 @@ import PostMessage from '../models/postMessage.js';
 import mongoose from 'mongoose';
 
 export const getPosts = async (req, res) => {
+	const { page } = req.query;
+
 	try {
-		const postMessages = await PostMessage.find();
-		res.status(200).json(postMessages);
+		const LIMIT = 8;
+		const startIndex = (Number(page) - 1) * LIMIT;
+		const total = await PostMessage.countDocuments({});
+		const posts = await PostMessage.find()
+			.sort({ _id: -1 })
+			.limit(LIMIT)
+			.skip(startIndex);
+		res.status(200).json({
+			data: posts,
+			currentPage: Number(page),
+			numberOfPages: Math.ceil(total / LIMIT),
+		});
+	} catch (error) {
+		console.log(error.message);
+		res.status(404).json({ message: error.message });
+	}
+};
+
+export const getPost = async (req, res) => {
+	const { id } = req.params;
+	try {
+		console.log(`${id} from controllers`);
+		const post = await PostMessage.findById(id);
+		console.log(`${post} from controllers`);
+		res.status(200).json(post);
+	} catch (error) {
+		console.log(error);
+		res.status(404).json({ message: error.message });
+	}
+};
+
+export const getPostsBySearch = async (req, res) => {
+	console.log('You hitted the search route');
+	const { searchQuery, tags } = req.query;
+	try {
+		const title = new RegExp(searchQuery, 'i');
+		const posts = await PostMessage.find({
+			$or: [{ title }, { tags: { $in: tags.split(',') } }],
+		});
+		res.status(200).json({ data: { data: posts } });
 	} catch (error) {
 		console.log(error.message);
 		res.status(404).json({ message: error.message });
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import {
 	getPosts,
+	getPost,
+	getPostsBySearch,
 	createPosts,
 	updatePost,
 	deletePost,
@@ -13,9 +15,10 @@ import authMiddleware from '../middleware/auth.js';
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/:id', getPost);
+router.get('/search', getPostsBySearch);
 router.post('/', authMiddleware, createPosts);
 router.patch('/:id', authMiddleware, updatePost);
 router.delete('/:id', authMiddleware, deletePost);
 router.patch('/:id/likePost', authMiddleware, likePost);
-
 export default router;
